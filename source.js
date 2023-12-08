81467ba6edcc39f74c6f31c29f622b6227d1b2ee
joejs92 <joejs_92@yahoo.com>
2023-12-08 11:40:47 -0500

    make grid print and user input prompt

--- source.js
+++ source.js
@@ -9,9 +9,58 @@ function Gameboard() {
             board[i].push("0");
         }
     }
-    for (let i = 0; i < board.length; i++) {
-        const printBoard=board[i].join(" ");
-        console.log(printBoard);
+
+    const getBoard = () => board;
+
+    const printBoard = () => {
+        for (let i = 0; i < board.length; i++) {
+            const printBoard=board[i].join(" ");
+            console.log(printBoard);
+        }
     }
+    return {getBoard, printBoard};
 }
+
+function GameController(
+    playerOneName = "Player One",
+    playerTwoName = "Player Two"
+) {
+    const board = Gameboard();
+    const players = [
+        {
+            name: playerOneName,
+            token: "X"
+        },
+        {
+            name: playerTwoName,
+            token: "O"
+        }
+    ];
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        if(activePlayer == players[0]){
+            activePlayer = players[1];
+        }
+        else{
+            activePlayer = players[0];
+        }
+    }
+
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn.`)
+        const choice = prompt("Choose a square.");
+        switchPlayerTurn();
+        printNewRound();
+        return choice;
+    }
+
+    printNewRound();
+
+    return {getActivePlayer};
+}
+const game = GameController();
 
